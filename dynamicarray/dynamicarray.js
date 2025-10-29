614f8e101299b915f7d52a822fbe135331709716
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-29 08:57:39 +0100

    implemented get method in DynamicArray

--- dynamicarray/dynamicarray.js
+++ dynamicarray/dynamicarray.js
@@ -4,9 +4,22 @@ export class DynamicArray {
   #staticArray;
   #size;
 
-  constructor(initialCapacity = 10) {
-    this.#staticArray = new StaticArray(initialCapacity);
+  constructor(capacity = 10) {
+    this.#staticArray = new StaticArray(capacity);
     this.#size = 0;
+  }
+
+  add(item) {
+    this.#size++;
+    this.#staticArray.set(this.#size - 1, item);
+  }
+
+  get(index) {
+    return this.#staticArray.get(index);
+  }
+
+  set(index, item) {
+    this.#staticArray.set(index, item);
   }
 
   size() {
--- dynamicarray/main.js
+++ dynamicarray/main.js
@@ -2,5 +2,9 @@ import { DynamicArray } from "./dynamicarray.js";
 
 let da = new DynamicArray();
 
+console.log(da.get(0));
+da.add("hest");
+
 console.log(da.size());
 console.log(da.capacity());
+console.log(da.get(0));
