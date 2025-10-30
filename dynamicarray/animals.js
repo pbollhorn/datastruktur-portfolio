63ab57e9f4e7b8cdefb589d5089dbc3053e8754d
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-30 08:34:07 +0100

    progression

--- dynamicarray/animals.js
+++ dynamicarray/animals.js
@@ -1,7 +1,6 @@
 import DynamicArray from "./dynamicarray.js";
 
 let da = new DynamicArray();
-// da.print();
 
 da.add("hest");
 da.add("giraf");
@@ -13,25 +12,8 @@ da.add("bænkebidder");
 da.add("rotte");
 da.add("mus");
 da.add("gråspurv");
-// da.print();
-
-da.add("aligator");
 da.print();
 
-da.insert(4, "øgle");
+
+da.set(10,"aligator");
 da.print();
-da.remove(4);
-da.print();
-
-console.log(da.get(10));
-console.log(da.get(0));
-
-
-let db = new DynamicArray(1);
-db.print();
-db.add("bøffel");
-db.print();
-db.add("løve");
-db.print();
-db.add("søløve");
-db.print();
