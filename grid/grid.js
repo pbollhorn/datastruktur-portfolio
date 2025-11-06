324901cef657307bccb173c1fe421d0ad4e999e6
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-06 09:50:17 +0100

    progression

--- grid/grid.js
+++ grid/grid.js
@@ -1,10 +1,14 @@
 export default class Grid {
-
-  #array;
+  #array; // Underlying 1D array
+  #rows; // Number of rows in the grids
+  #cols; // Number of columns in the grid
 
   constructor(rows, cols) {
     this.#array = [];
-    for (let i = 0; i < rows * cols; i++) {
+    this.#rows = rows;
+    this.#cols = cols;
+    const length = rows * cols;
+    for (let i = 0; i < length; i++) {
       this.#array[i] = undefined;
     }
   }
@@ -13,5 +17,9 @@ export default class Grid {
     for (let i = 0; i < this.#array.length; i++) {
       console.log(this.#array[i]);
     }
+  }
+
+  get({ row, col }) {
+    const index = row * this.#cols + col;
   }
 }
