48590b6802d166056633cf0d95f16ce30d10b888
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-09 14:05:05 +0100

    progression

--- doublylinkedlist/main.js
+++ doublylinkedlist/main.js
@@ -13,17 +13,7 @@ myList.addLast("hedder");
 myList.addLast("Kaj.");
 myList.printList();
 
-console.log(myList.removeFirst());
-myList.printList();
-
-// console.log(myList.get(5));
-
-// // Using the iterator in a `for...of` loop
-// console.log("Iterating over the linked list:");
-// for (const node of myList) {
-//   console.log(node.data); // Output: 10, 20, 30
-// }
-
-// // Alternatively, you can spread the nodes into an array
-// console.log("Linked list as an array:");
-// console.log([...myList].map((node) => node.data)); // Output: [10, 20, 30]
+console.log("Itererer over listen med et for...of loop:");
+for (const node of myList) {
+  console.log(node.data);
+}
