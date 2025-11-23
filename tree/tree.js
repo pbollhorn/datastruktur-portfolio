8abc92f3e24dd2b183439e3f9a61a3c2189182df
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-23 21:19:38 +0100

    created tree.js

---
+++ tree/tree.js
@@ -0,0 +1,15 @@
+export default class Tree {
+  #root;
+
+
+  // udskriver hele træet i consollen.
+  dump() {}
+
+  // opretter en ny node med den givne value, og tilføjer den et sted i træet - du bestemmer selv hvor!
+  addValue(value) {}
+
+  // leder efter den givne value i træet, og returnerer den (første) Node der har den
+  findValue(value) {}
+
+  removeValue(value) {}
+}
