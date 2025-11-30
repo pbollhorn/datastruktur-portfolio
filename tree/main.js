0bab274d6efd60476cd0a2592678b568cc22fb3f
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-30 13:30:38 +0100

    progression

--- tree/main.js
+++ tree/main.js
@@ -12,21 +12,11 @@ const tree = new Tree(nodeA);
 nodeA.appendChild(nodeB);
 nodeA.appendChild(nodeC);
 nodeB.appendChild(nodeD);
+nodeD.appendChild(nodeE);
+nodeD.appendChild(nodeF);
 
 console.log(tree.findValue("D"));
 console.log(tree.findValue("Value not in tree"));
 
 console.log("\nPrint tree as a list:");
 tree.printTree();
-
-// const node1 = new Node("node1");
-
-// console.log(node1.firstChild());
-// console.log(node1.lastChild());
-// console.log(node1.hasChildNodes());
-
-// const node2 = new Node("node2");
-// node1.appendChild(node2);
-// console.log(node1.firstChild());
-// console.log(node1.lastChild());
-// console.log(node1.hasChildNodes());
